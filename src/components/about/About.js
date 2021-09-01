dacf815c86e23bcd0c6785d32ee6300971411a7c
Chris S <65880029+JorganBonstragel@users.noreply.github.com>
2021-09-01 01:18:03 -0400

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -19,7 +19,7 @@ function About (props) {
                     {props.data[0].bio}
                 </p>
                 <div className="About-footer">
-                    <a href={props.data[0].resume} download="filename" className="button-link">resume</a>
+                    <a href={props.data[0].resume} download="Chris Sampson: Resume" className="button-link">resume</a>
                     <a href="https://github.com/ChrsSampson" className="link"><GithubFill className="icon" size={35} /></a>
                     <a href="https://www.linkedin.com/in/chris-s-558341202/" className="link"><LinkedInV1Fill className="icon"  size={35} /></a>
                 </div>
